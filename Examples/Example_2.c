ebeed563f36cdc16f4804deb0725a62af89aca34
chankitmittal <mittalchankit@gmail.com>
2026-05-21 18:48:48 +0530

    Tool example files

---
+++ Examples/Example_2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main() 
+{
+    int max_num = 100;
+    for(int i = 0; i < max_num; i++)
+    {
+        printf("Iteration number - %d, Max iteration number - %d\n", i, max_num);
+    }
+}
