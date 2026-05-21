8a90a1179447ea137c231bee14b9715f509baf30
chankitmittal <mittalchankit@gmail.com>
2026-05-21 18:47:58 +0530

    Create Example_1.c

---
+++ Examples/Example_1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int max_num = 100;
+    for(int i = 0; i < max_num; i++)
+    {
+        printf("Iteration number - %d\n", i);
+    }
+    return 0;
+}
